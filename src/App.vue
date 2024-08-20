4cc16228c09236f10d68c4e5e9e89f2ef9bdc9ae
duchi321 <sqaz0502@gmail.com>
2024-08-20 15:14:49 +0800

    create sign up feature

--- src/App.vue
+++ src/App.vue
@@ -1,47 +1,65 @@
 <script setup>
-import HelloWorld from './components/HelloWorld.vue'
-import TheWelcome from './components/TheWelcome.vue'
+import { ref } from 'vue'
+import axios from 'axios'
+
+const api = 'https://todolist-api.hexschool.io'
+const userdata = ref({
+  nickname: '',
+  password: '',
+  email: ''
+})
+const usersignin = ref('')
+
+const signUp = async () => {
+  console.log(`${api}/users/sign_up`)
+  try {
+    const res = await axios.post(`${api}/users/sign_up`, userdata.value)
+    console.log(res)
+    usersignin.value = res.data.uid
+  } catch (error) {
+    console.log(error.response.data.message)
+  }
+}
 </script>
 
 <template>
-  <header>
-    <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
-
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
-    </div>
-  </header>
-
-  <main>
-    <TheWelcome />
-  </main>
+  <div class="container">
+    <header>
+      <h1>week two homework</h1>
+    </header>
+    <main>
+      <div>
+        <h2>註冊</h2>
+        <label for="nickname">帳號: </label>
+        <input type="text" placeholder="user" v-model="userdata.nickname" /><br />
+        <label for="password">密碼: </label>
+        <input type="text" placeholder="password" v-model="userdata.password" /><br />
+        <label for="email">信箱: </label>
+        <input type="email" placeholder="email" v-model="userdata.email" /><br />
+        <button type="button" @click="signUp">註冊</button>
+        <p>UID: {{ usersignin }}</p>
+        {{ userdata }}
+      </div>
+    </main>
+  </div>
 </template>
 
 <style scoped>
+.container {
+  display: flex;
+  flex-direction: column;
+}
 header {
-  line-height: 1.5;
+  /* outline: 3px solid red; */
+  width: 100%;
+  height: 45px;
+  background-color: turquoise;
+  box-shadow: 3px 1px 3px 1px rgba(0, 25, 55, 0.6);
 }
-
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
-}
-
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
-
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
+h1 {
+  /* outline: 3px solid red; */
+  margin: 0;
+  padding: 0 0 0 15px;
+  line-height: 45px;
 }
 </style>
